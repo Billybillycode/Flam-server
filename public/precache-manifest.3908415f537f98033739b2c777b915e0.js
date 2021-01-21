36c9ba6128d42d00d32b0ce0c755a89b7a5481a1
Gwerborc'h Mahéo <gwerborch.maheo.com>
2021-01-21 10:08:00 +0100

    Your commit message4

---
+++ public/precache-manifest.3908415f537f98033739b2c777b915e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0933bf17b54c9f2c6728b87ac5812e2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "568312cd382e62f09886",
+    "url": "/static/css/main.447f1440.chunk.css"
+  },
+  {
+    "revision": "a9c06f4db228dde29fed",
+    "url": "/static/js/2.c122b724.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.c122b724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568312cd382e62f09886",
+    "url": "/static/js/main.de70941f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
